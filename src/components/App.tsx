e8f5cd6de948cad074e44c0a91b86e49891f63d9
topether21 <topeher21@gmail.com>
2023-04-03 15:32:14 -0600

    init project

---
+++ src/components/App.tsx
@@ -0,0 +1,20 @@
+import Avatar from 'components/Avatar'
+
+function App() {
+  const a = {}
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <div className="h-screen sm:pt-24 sm:pb-40 lg:pt-40 lg:pb-48">
+        <div className="relative px-4 mx-auto max-w-7xl sm:static sm:px-6 lg:px-8">
+          <div className="sm:max-w-lg">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Welcome!
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+/** @type {import('vite').UserConfig} */
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// https://vitejs.dev/config https://vitest.dev/config
+export default {
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    setupFiles: '.vitest/setup',
+    include: ['**/test.{ts,tsx}']
+  }
+}
